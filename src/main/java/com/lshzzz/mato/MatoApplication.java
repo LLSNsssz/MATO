7680881891ca40a24bb1237d97d03cf38456ae6e
LLSNsssz <seungjang93@gmail.com>
2025-02-14 20:20:42 +0900

    Init: 프로젝트 초기 설정

---
+++ src/main/java/com/lshzzz/mato/MatoApplication.java
@@ -0,0 +1,13 @@
+package com.lshzzz.mato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lshzzz/mato/MatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lshzzz.mato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
